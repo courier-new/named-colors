45c9a59b7535759668759729d80c0d278c37c0af
Kelli Rockwell <kelli@sourcegraph.com>
2023-09-01 13:49:08 -0700

    Document useFavorites

--- src/components/useFavorites.ts
+++ src/components/useFavorites.ts
@@ -1,8 +1,22 @@
 import { $, type QRL, useVisibleTask$, useContext } from "@builder.io/qwik";
 import { GlobalStore } from "~/context";
 
-const KEY = "favorites";
+/** Local storage key for persisting user's favorite colors. */
+const FAVORITES_STORAGE_KEY = "favorites";
 
+/**
+ * useFavorites consolidates logic for managing a user's set of favorite colors. It
+ * returns a tuple containing:
+ * - An array of the user's current favorite colors.
+ * - A function to add a color by name to the user's favorites.
+ * - A function to remove a color by name from the user's favorites.
+ *
+ * The user's favorites are persisted to local storage and can be accessed with this hook
+ * or via the `GlobalStore` context across the application.
+ *
+ * @returns A tuple containing the user's favorite colors, a function to add a favorite,
+ * and a function to remove a favorite.
+ */
 export function useFavorites(): [
   favorites: string[],
   addFavorite$: QRL<(name: string) => void>,
@@ -14,13 +28,16 @@ export function useFavorites(): [
     () => {
       if (globalStore.favorites.length) return;
       try {
-        const favoritesJSON = window.localStorage.getItem(KEY);
+        const favoritesJSON = window.localStorage.getItem(
+          FAVORITES_STORAGE_KEY
+        );
         globalStore.favorites = favoritesJSON ? JSON.parse(favoritesJSON) : [];
       } catch (error) {
         console.warn("Could not load favorites from local storage", error);
         globalStore.favorites = [];
       }
     },
+    // Set the user's favorites in app state on page load.
     { strategy: "document-ready" }
   );
 
@@ -28,7 +45,10 @@ export function useFavorites(): [
     const set = new Set([...globalStore.favorites, name]);
     globalStore.favorites = Array.from(set);
     try {
-      window.localStorage.setItem(KEY, JSON.stringify(Array.from(set)));
+      window.localStorage.setItem(
+        FAVORITES_STORAGE_KEY,
+        JSON.stringify(Array.from(set))
+      );
     } catch (error) {
       console.warn("Could not save favorite to local storage", error);
     }
@@ -38,7 +58,10 @@ export function useFavorites(): [
     const newFavorites = globalStore.favorites.filter((n) => n !== name);
     globalStore.favorites = newFavorites;
     try {
-      window.localStorage.setItem(KEY, JSON.stringify(newFavorites));
+      window.localStorage.setItem(
+        FAVORITES_STORAGE_KEY,
+        JSON.stringify(newFavorites)
+      );
     } catch (error) {
       console.warn("Could not remove favorite from local storage", error);
     }
